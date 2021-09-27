5a08cb32feb7cdd19c45e9e78a582abdc9477473
Nilsa <nilsa.jeandot@gmail.com>
2021-09-27 18:09:36 +0200

    addStyle and home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailsHikesComponent } from './components/details-hikes/details-hikes.component';
 import {HikesComponent} from "./components/hikes/hikes.component";
+import {HomeComponent} from "./components/home/home.component";
 
 const routes: Routes = [
   {path:'hikes', component:HikesComponent},
-  {path:'details/:id', component:DetailsHikesComponent}
+  {path:'details/:id', component:DetailsHikesComponent},
+  {path:'home', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { HikesComponent } from './components/hikes/hikes.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import { DetailsHikesComponent } from './components/details-hikes/details-hikes.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HikesComponent,
     DetailsHikesComponent,
+    NavbarComponent,
+    HomeComponent,
   ],
     imports: [
         BrowserModule,
