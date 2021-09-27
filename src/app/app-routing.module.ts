86a958186f2243dd6ed8386bdc7a0dd1b995e8e4
Nilsa <nilsa.jeandot@gmail.com>
2021-09-27 13:10:05 +0200

    add component hikes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HikesComponent} from "./components/hikes/hikes.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'hikes', component:HikesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HikesComponent } from './components/hikes/hikes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HikesComponent
   ],
   imports: [
     BrowserModule,
